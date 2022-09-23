d098cab26b42b35310e303dcfda1e45e6ba98311
Madushan Chamara <shanchamara.lk@gmail.com>
2022-09-23 11:32:38 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
        
-        How to Learn React
+         Learn React
         
       </header>
     </div>
